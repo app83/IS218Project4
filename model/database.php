1d2b03e08e69bdb211876238570f835db89adbf9
Ami Patel <app83@njit.edu>
2020-12-15 12:24:09 -0500

    changing to classes

--- model/database.php
+++ model/database.php
@@ -1,14 +1,26 @@
 <?php
+class Database {
+    private static $dsn = 'mysql:host=sql1.njit.edu;dbname=app83';
+    private static $username = 'app83';
+    private static $password = 'Dreambig1!';
+    private static $db;
 
-$username = 'app83';
-$password = 'Dreambig1!';
-$dsn = 'mysql:host=sql1.njit.edu;dbname=app83';
+    private function __construct() {}
 
-try {
-    $db = new PDO($dsn, $username, $password);
-} catch (PDOException $error) {
-    $error_message = $error->getMessage();
-    include('../errors/database_error.php');
-    exit();
+    public static function getDB () {
+        if (!isset(self::$db)) {
+            try {
+                self::$db = new PDO(self::$dsn,
+                    self::$username,
+                    self::$password);
+            } catch (PDOException $e) {
+                $error_message = $e->getMessage();
+                include('../errors/database_error.php');
+                exit();
+            }
+        }
+        return self::$db;
+    }
 }
 ?>
+
